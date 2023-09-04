8ec5827dc670e55d736b7bfa989edc0b6bb68507
DavidGonyAtos <36034760+DavidGonyAtos@users.noreply.github.com>
2023-09-04 09:27:05 +0000

    Ajout 'un commentaire tout en haut de program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Bases;
 
-// See https://aka.ms/new-console-template for more information
+// Voir dépot correspondant dans: https://github.com/DavidGonyAtos/Gyt?search=1  
 Console.WriteLine("Hello, World!");
 
 Base uneBase= new Base();   // premières modifications pour premier commit avec création Bases.cs
