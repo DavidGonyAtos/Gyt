89a08dd53a8cb9a7606b69035229da09bef8c06c
DavidGonyAtos <36034760+DavidGonyAtos@users.noreply.github.com>
2023-09-04 09:07:09 +0000

    Ajout du fichier bases.cs et modification du Progem.cs

---
+++ CSHARP Perso/bases.cs
@@ -0,0 +1,12 @@
+
+// premières créations pour premier commit avec création Bases.cs
+
+namespace Bases;
+
+internal class Base
+{
+    public void write()
+    {
+        Console.WriteLine("C'est partit, on est dans Bases.cs");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Bases;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Base uneBase= new Base();   // premières modifications pour premier commit avec création Bases.cs
+uneBase.write();            // premières modifications pour premier commit avec création Bases.cs
