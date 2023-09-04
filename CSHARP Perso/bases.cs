0425098719f7c126f436e62428b227c9beaa60ff
DavidGonyAtos <36034760+DavidGonyAtos@users.noreply.github.com>
2023-09-04 09:35:20 +0000

    Premier commit sur refonte avec juste un add sur bases.cs mais pas sur Program.cs

--- CSHARP Perso/bases.cs
+++ CSHARP Perso/bases.cs
@@ -9,4 +9,8 @@ internal class Base
     {
         Console.WriteLine("C'est partit, on est dans Bases.cs");
     }
+    public void writes()
+    {
+        Console.WriteLine("modification de Branche refonte sur bases.cs");
+    }
 }
